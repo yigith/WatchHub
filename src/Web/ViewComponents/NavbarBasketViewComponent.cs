ba9db6c574a8004c614f87750d6b3f6fef35e3de
yigith <yigith1@gmail.com>
2023-09-18 12:53:08 +0300

    NavbarBasketViewComponent

---
+++ src/Web/ViewComponents/NavbarBasketViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.ViewComponents
+{
+    public class NavbarBasketViewComponent : ViewComponent
+    {
+        // todo: yarın IBasketViewModelService üzerinden BasketViewModel türünde
+        // sepeti buraya getireceğiz ve öğelerinin sayısını View'a aktaracağız
+        // ki o View da navbarın sağ üstüne yerleştirilecek view component ile..
+        public NavbarBasketViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
