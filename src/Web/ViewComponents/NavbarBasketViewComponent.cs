d1db58f521591cc5eb2df62a3b36cf92fed69edf
yigith <yigith1@gmail.com>
2023-09-19 11:50:40 +0300

    navbarbasket

--- src/Web/ViewComponents/NavbarBasketViewComponent.cs
+++ src/Web/ViewComponents/NavbarBasketViewComponent.cs
@@ -4,16 +4,17 @@ namespace Web.ViewComponents
 {
     public class NavbarBasketViewComponent : ViewComponent
     {
-        // todo: yarın IBasketViewModelService üzerinden BasketViewModel türünde
-        // sepeti buraya getireceğiz ve öğelerinin sayısını View'a aktaracağız
-        // ki o View da navbarın sağ üstüne yerleştirilecek view component ile..
-        public NavbarBasketViewComponent()
+        private readonly IBasketViewModelService _basketViewModelService;
+
+        public NavbarBasketViewComponent(IBasketViewModelService basketViewModelService)
         {
+            _basketViewModelService = basketViewModelService;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var basket = await _basketViewModelService.GetBasketViewModelAsync();
+            return View(basket);
         }
     }
 }
